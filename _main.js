3cc562a3bc5b304bd13c412019f32f3634b6ccd7
thecichos <thecichos@gmail.com>
2016-11-01 11:04:35 +0100

    some more shit

--- _main.js
+++ _main.js
@@ -2,6 +2,23 @@ $('body').append("	<div id='container'></div>");
 if ($("#container").children().length == 0) {
 	$("#container").append("<div class='item'></div>")
 }
-for (var i = 0; i < 15; i++) {
-	
+
+function connect(n1) {
+	$.ajax({
+		url: 'php/_gen.php',
+		type: "POST",
+		data: {name: n1}
+	})
+	.done(function(result) {
+		console.log("success " + result);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
 }
+
+console.log(connect("yeah"));
